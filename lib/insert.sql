8a489855424854adbf138f3a112f2fc63f8e2a98
Kelin Field <kfield111@hotmail.com>
2020-10-05 23:12:38 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age , gender, color, temperament. alive) VALUES ("Mr. Chocolate", 5, "male", "brown", "sweet", true)
+INSERT INTO bears (name, age , gender, color, temperament. alive) VALUES ("Rowdy", 6, "male", "brown", "sweet", true)
+INSERT INTO bears (name, age , gender, color, temperament. alive) VALUES ("Tabitha", 5, "female", "brown", "sweet", true)
+INSERT INTO bears (name, age , gender, color, temperament. alive) VALUES ("Sergeant Brown", 3, "male", "brown", "sweet", true)
+INSERT INTO bears (name, age , gender, color, temperament. alive) VALUES ("Melissa", 8, "female", "brown", "sweet", true)
+INSERT INTO bears (name, age , gender, color, temperament. alive) VALUES ("Grinch", 5, "male", "brown", "sweet", true)
+INSERT INTO bears (name, age , gender, color, temperament. alive) VALUES ("Wendy", 7, "female", "brown", "sweet", true)
+INSERT INTO bears (name, age , gender, color, temperament. alive) VALUES ("null", 12, "male", "dark brown", "hungry", false)
